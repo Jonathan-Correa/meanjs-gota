72ea3d815102839f7a53d77d1a3d44be97063007
Adolfo Cardona <acardona1987@gmail.com>
2020-02-12 14:42:41 -0500

    Initial commit

---
+++ modules/users/client/config/users-admin.client.routes.js
@@ -0,0 +1,64 @@
+(function () {
+  'use strict';
+
+  // Setting up route
+  angular
+    .module('users.admin.routes')
+    .config(routeConfig);
+
+  routeConfig.$inject = ['$stateProvider'];
+
+  function routeConfig($stateProvider) {
+    $stateProvider
+      .state('admin.users', {
+        url: '/users',
+        templateUrl: '/modules/users/client/views/admin/list-users.client.view.html',
+        controller: 'UserListController',
+        controllerAs: 'vm'
+      })
+      .state('admin.user', {
+        url: '/users/:userId',
+        templateUrl: '/modules/users/client/views/admin/view-user.client.view.html',
+        controller: 'UserController',
+        controllerAs: 'vm',
+        resolve: {
+          userResolve: getUser
+        },
+        data: {
+          pageTitle: '{{ userResolve.displayName }}'
+        }
+      })
+      .state('admin.user-create', {
+        url: '/users/create',
+        templateUrl: '/modules/users/client/views/admin/create-user.client.view.html',
+        controller: 'UserController',
+        controllerAs: 'vm',
+        resolve: {
+          userResolve: getUser
+        },
+        data: {
+          pageTitle: 'Crear usuario'
+        }
+      })
+      .state('admin.user-edit', {
+        url: '/users/:userId/edit',
+        templateUrl: '/modules/users/client/views/admin/edit-user.client.view.html',
+        controller: 'UserController',
+        controllerAs: 'vm',
+        resolve: {
+          userResolve: getUser
+        },
+        data: {
+          pageTitle: '{{ userResolve.displayName }}'
+        }
+      });
+
+    getUser.$inject = ['$stateParams', 'AdminService'];
+
+    function getUser($stateParams, AdminService) {
+      return AdminService.query({
+        userId: $stateParams.userId
+      }).$promise;
+    }
+  }
+}());
---
+++ modules/users/server/controllers/admin.server.controller.js
@@ -0,0 +1,128 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var path = require('path'),
+  mongoose = require('mongoose'),
+  User = mongoose.model('User'),
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
+
+/**
+ * Show the current user
+ */
+exports.read = function (req, res) {
+  res.json(req.model);
+};
+
+/**
+ * Create User
+ */
+exports.create = function (req, res) {
+
+  console.log('req.body.username');
+  console.log(req.body.username);
+  console.log('req.body.username');
+
+  var user = new User({
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    displayName: req.body.firstName + ' ' + req.body.lastName,
+    provider: req.body.provider,
+    email: req.body.email,
+    roles: req.body.roles
+  });
+
+  user.save(function (err) {
+    if (err) {
+      console.log(err);
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    }
+
+    console.log('user');
+    console.log(user);
+    console.log('user');
+
+    res.json(user);
+  });
+};
+
+/**
+ * Update a User
+ */
+exports.update = function (req, res) {
+  var user = req.model;
+
+  // For security purposes only merge these parameters
+  user.firstName = req.body.firstName;
+  user.lastName = req.body.lastName;
+  user.displayName = user.firstName + ' ' + user.lastName;
+  user.roles = req.body.roles;
+
+  user.save(function (err) {
+    if (err) {
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    }
+
+    res.json(user);
+  });
+};
+
+/**
+ * Delete a user
+ */
+exports.delete = function (req, res) {
+  var user = req.model;
+
+  user.remove(function (err) {
+    if (err) {
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    }
+
+    res.json(user);
+  });
+};
+
+/**
+ * List of Users
+ */
+exports.list = function (req, res) {
+  User.find({}, '-salt -password -providerData').sort('-created').populate('user', 'displayName').exec(function (err, users) {
+    if (err) {
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    }
+
+    res.json(users);
+  });
+};
+
+/**
+ * User middleware
+ */
+exports.userByID = function (req, res, next, id) {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'User is invalid'
+    });
+  }
+
+  User.findById(id, '-salt -password -providerData').exec(function (err, user) {
+    if (err) {
+      return next(err);
+    } else if (!user) {
+      return next(new Error('Failed to load user ' + id));
+    }
+
+    req.model = user;
+    next();
+  });
+};
